45d267b7841c6513712c270f2b98b3b4d5a48d2d
kolyagithub <alimov.kolya@gmail.com>
2018-03-01 13:07:33 +0500

    add use SocketIO as Client

--- src/main/kotlin/blog/Main.kt
+++ src/main/kotlin/blog/Main.kt
@@ -4,20 +4,31 @@ import io.javalin.ApiBuilder.*
 import io.javalin.Javalin
 import com.corundumstudio.socketio.listener.*
 import com.corundumstudio.socketio.*
+import io.javalin.Handler
 import mu.KotlinLogging
+
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import org.json.JSONException
+import org.json.JSONObject
 
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
 
+    /*
+        * Use as server
+     */
+    /*
     val config: Configuration = Configuration()
-    val host: String = "192.168.1.188"
-    val port: Int = 7001
+    val host: String = "127.0.0.1"
+    val port: Int = 3004
     config.setHostname(host)
     config.setPort(port)
 
 
-    val server = SocketIOServer(config)
+    val server = SocketIOClient (config)
 
     // Without NSP
     val onData1 = DataListener<ChatObject> { client, data, ackRequest ->
@@ -26,11 +37,12 @@ fun main(args: Array<String>) {
     server.addEventListener("chatevent", ChatObject::class.java, onData1)
 
     // With NSP
-    val chat1namespace = server.addNamespace("/nsp1")
+    //val chat1namespace = server.addNamespace("/nsp1")
+    val chat1namespace = server.addNamespace("70204c8e-d46f-4273-b920-bf879f2b16ed")
 
     val onData2 = DataListener<Any> { client, data, ackRequest ->
         println(data)
-        chat1namespace.getBroadcastOperations().sendEvent("message", data)
+        chat1namespace.getBroadcastOperations().sendEvent("data", data)
     }
 
     chat1namespace.addEventListener("message", Any::class.java, onData2)
@@ -58,5 +70,41 @@ fun main(args: Array<String>) {
         }
 
     }
+*/
+
+
+    /*
+        * Use as client
+     */
+    val socket = IO.socket("http://127.0.0.1:3004/70204c8e-d46f-4273-b920-bf879f2b16ed")
+    socket.connect()
+            .on(Socket.EVENT_CONNECT, { println("connected") })
+            .on(Socket.EVENT_DISCONNECT, { println("disconnected") })
+            .on("data", { args ->
+                val obj = args[0] as JSONObject
+                println("TSP: ${obj["tsp"]}")
+                println("VAL_1: ${obj["val1"]}")
+                println("VAL_2: ${obj["val2"]}")
+
+                val iter = obj.keys()
+                while (iter.hasNext()) {
+                    val key = iter.next()
+                    try {
+                        println("AAAAAAAAAAAAAAAA: $key")
+                        //val pos = obj.getJSONObject(key)
+
+                        //Main thread
+                        /*Handler(Looper.getMainLooper()).post {
+                            if (markers[key] == null) {
+                                markers[key] = mMap.addMarker(marker.position(LatLng(pos.getDouble("lat"), pos.getDouble("lng"))))
+                            } else {
+                                markers[key]!!.position = LatLng(pos.getDouble("lat"), pos.getDouble("lng"))
+                            }
+                        }*/
+                    } catch (e: JSONException) {
+                        // Something went wrong!
+                    }
+                }
+            })
 
 }
